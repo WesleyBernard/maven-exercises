22d4475d879b9fe34abdceee3e07186c212b797b
Wesley Bernard <wesley.bernard64@gmail.com>
2022-03-21 13:01:57 -0400

    Refactoring

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,7 +6,8 @@ public class main {
         System.out.print("Enter Something: ");
         String strong = daBoi.nextLine();
         System.out.println("you entered : " + strong);
-        System.out.println(StringUtils.isNumeric(strong));
+        String numeric = StringUtils.isNumeric(strong) ? "That's numeric!" : "That's not numeric!";
+        System.out.println(numeric);
         System.out.println(StringUtils.swapCase(strong));
         System.out.println(StringUtils.reverse(strong));
     }
